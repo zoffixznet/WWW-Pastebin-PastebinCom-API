8a195a500a0ee303cc9a487e78943530096ab2b1
Zoffix Znet <cpan@zoffix.com>
2013-12-03 21:33:55 -0500

    first commit

---
+++ examples/retrieve.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use lib qw(../lib lib);
+use WWW::Pastebin::PastebinCom::API;
+
+@ARGV or die "Usage: $0 [paste URL or ID]\n";
+
+my $bin = WWW::Pastebin::PastebinCom::API->new;
+my $paste = $bin->get_paste( shift )
+    or die "$bin";
+
+print "$paste\n";
