04fd5227afb2a13f22fe53719d3ac5a06dfb2fe4
Zoffix Znet <cpan@zoffix.com>
2015-03-13 04:44:03 -0400

    Listed new prefered bug tracker and repo

--- lib/WWW/Pastebin/PastebinCom/API.pm
+++ lib/WWW/Pastebin/PastebinCom/API.pm
@@ -1764,86 +1764,31 @@ word C<Error: > is added to the error message.
 The C<examples/> directory of this distribution contains two example
 scripts: one for pasting a file and another for retrieving a paste.
 
+=head1 REPOSITORY
+
+Fork this module on GitHub:
+L<https://github.com/zoffixznet/WWW-Pastebin-PastebinCom-API>
+
+=head1 BUGS
+
+To report bugs or request features, please use
+L<https://github.com/zoffixznet/WWW-Pastebin-PastebinCom-API/issues>
+
+If you can't access GitHub, you can email your request
+to C<bug-www-pastebin-pastebincom-api at rt.cpan.org>
+
 =head1 AUTHOR
 
-Zoffix Znet, C<< <cpan at zoffix.com> >>
+Zoffix Znet C<zoffix at cpan.org>, (L<http://zoffix.com/>)
 
 =head1 CONTRIBUTORS
 
 Philipp Hamer, L<https://github.com/ponzellus>
 
-=head1 BUGS
+=head1 LICENSE
 
-Please report any bugs or feature requests to C<bug-www-pastebin-pastebincom-api at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=WWW-Pastebin-PastebinCom-API>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc WWW::Pastebin::PastebinCom::API
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=WWW-Pastebin-PastebinCom-API>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/WWW-Pastebin-PastebinCom-API>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/WWW-Pastebin-PastebinCom-API>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/WWW-Pastebin-PastebinCom-API/>
-
-=back
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 Zoffix Znet.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+You can use and distribute this module under the same terms as Perl itself.
+See the C<LICENSE> file included in this distribution for complete
+details.
 
 =cut
